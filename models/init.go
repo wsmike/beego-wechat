dfda0c554abf142110fc6bc4f9bd3c039464bf4f
wsmike <2393209180@qq.com>
2018-10-16 16:09:46 +0800

    Create init.go

---
+++ models/init.go
@@ -0,0 +1,38 @@
+package models
+
+import (
+    "net/url"
+
+    "github.com/astaxie/beego"
+    "github.com/astaxie/beego/orm"
+    _ "github.com/go-sql-driver/mysql"
+)
+
+func Init() {
+    dbhost := beego.AppConfig.String("db.host")
+    dbport := beego.AppConfig.String("db.port")
+    dbuser := beego.AppConfig.String("db.user")
+    dbpassword := beego.AppConfig.String("db.password")
+    dbname := beego.AppConfig.String("db.name")
+    timezone := beego.AppConfig.String("db.timezone")
+    if dbport == "" {
+        dbport = "3306"
+    }
+    dsn := dbuser + ":" + dbpassword + "@tcp(" + dbhost + ":" + dbport + ")/" + dbname + "?charset=utf8"
+   
+
+    if timezone != "" {
+        dsn = dsn + "&loc=" + url.QueryEscape(timezone)
+    }
+    orm.RegisterDataBase("default", "mysql", dsn)
+
+    orm.RegisterModel(new(User))
+
+    if beego.AppConfig.String("runmode") == "dev" {
+        orm.Debug = true
+    }
+}
+
+func TableName(name string) string {
+    return beego.AppConfig.String("db.prefix") + name
+}
